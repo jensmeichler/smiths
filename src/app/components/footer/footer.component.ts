5544614d28f54eda282ff1cd5d54005c14734493
Jens Meichler <j.meichler@rebuy.com>
2024-04-26 21:21:10 +0200

    feat: signature

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,21 @@
 import {Component, HostBinding} from '@angular/core';
+import {fromEvent, map, startWith} from 'rxjs';
+import {AsyncPipe, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    AsyncPipe
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
   @HostBinding('style.background-image')
   background = 'url("assets/img/hero.jpg")';
+
+  narrow$ = fromEvent(window, 'resize')
+    .pipe(startWith(undefined), map(() => window.innerWidth < 420));
 }
